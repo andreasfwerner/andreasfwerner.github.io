7fe0584e3b014a1b0eea7f252472e44f608f8fb0
Andreas Faye Werner <wernerbros@DESKTOP-TOVKD4B.localdomain>
2022-12-02 20:26:35 +0100

    second commit

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,61 @@
 <template>
-    <div>
-        well. hello there
+    <div class="navbar">
+        <div class="filler">
+            <p>andreasfwerner</p>
+        </div>
+        <div class="flexwrap">
+            <NavBarButton></NavBarButton>
+            <NavBarButton></NavBarButton>
+            <NavBarButton></NavBarButton>
+            <NavBarButton></NavBarButton>
+        </div>
     </div>
 </template>
 
 <script>
+import NavBarButton from './NavBarButton.vue'
 export default{
-    name: 'NavBar'
+    name: 'NavBar',
+    components: {
+        NavBarButton
+  }
 }
 </script>
+
+<style>
+    *{
+        margin:0;
+        padding:0;
+
+    }
+    .navbar{
+        width: 100%;
+        height: 7em;
+        background-color: green;
+        display: flex;
+
+    }
+    .filler{
+        width: 30%;
+        height: 100%;
+        background-color: aqua;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .flexwrap{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+        width: 70%;
+        height: 100%;
+        background-color: aliceblue;
+    }
+    p{
+        font-size: large;
+        font-family: 'Courier New', Courier, monospace;
+        font-weight: bold;
+    }
+
+</style>
---
+++ src/components/NavBarButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="buttonStyle">
+
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'NavBar'
+}
+</script>
+
+<style>
+    .buttonStyle{
+        width: 15%;
+        height: 70%;
+        background-color: black;
+    }
+</style>
